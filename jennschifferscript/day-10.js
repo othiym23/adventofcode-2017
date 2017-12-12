1d2ed1546dbb9b9fd5e7128218bd199cbcac2f6c
Forrest L Norvell <ogd@aoaioxxysz.net>
2017-12-11 17:07:02 -0800

    cleaned up day 10
    including restoring the existence of part 1

--- jennschifferscript/day-10.js
+++ jennschifferscript/day-10.js
@@ -1,22 +1,37 @@
 const assert = require('assert')
 const readFileSync = require('fs').readFileSync
+const resolve = require('path').resolve
+
+const provided = readFileSync(resolve(__dirname, '../inputs/day-10.input'), 'ascii').trim()
 
 const SIZE = 256
 const ROUNDS = 64
 
-assert.equal(run(''), 'a2582a3a0e66e6e86e3812dcb672a272')
-assert.equal(run('AoC 2017'), '33efeb34ea91902bb2f59c9920caa6cd')
-assert.equal(run('1,2,3'), '3efbe78a8d82f29979031a4aa0b16a9d')
-assert.equal(run('1,2,4'), '63960835bcdc130f0b66d7ff4f6a5a8e')
+assert.equal(run1('3,4,1,5', 5), 12)
 
-const provided = readFileSync('./day-10.txt', 'ascii').trim()
-console.log('hash value is', run(provided))
+console.log('first hash code is', run1(provided, SIZE))
 
-function run (input) {
-  return toOutput(aFrayedKnot(prepare(input)))
+assert.equal(run2(''), 'a2582a3a0e66e6e86e3812dcb672a272')
+assert.equal(run2('AoC 2017'), '33efeb34ea91902bb2f59c9920caa6cd')
+assert.equal(run2('1,2,3'), '3efbe78a8d82f29979031a4aa0b16a9d')
+assert.equal(run2('1,2,4'), '63960835bcdc130f0b66d7ff4f6a5a8e')
+
+console.log('second hash value is', run2(provided))
+
+function run1 (input, size) {
+  const ring = aFrayedKnot(prepare1(input), size, 1)
+  return ring[0] * ring[1]
 }
 
-function prepare (input) {
+function prepare1 (input) {
+  return input.split(',').map(v => parseInt(v, 10))
+}
+
+function run2 (input) {
+  return toOutput(aFrayedKnot(prepare2(input), SIZE, ROUNDS))
+}
+
+function prepare2 (input) {
   assert.equal(typeof input, 'string')
   return input.split('').map(e => e.codePointAt(0)).concat([17, 31, 73, 47, 23])
 }
@@ -31,16 +46,16 @@ function toOutput (knot) {
   return Buffer.from(dense).toString('hex')
 }
 
-function aFrayedKnot (lengths) {
-  const ring = buildRing(SIZE)
+function aFrayedKnot (lengths, size, rounds) {
+  const ring = buildRing(size)
 
   let pos = 0
   let skipSize = 0
 
-  for (let i = 0; i < ROUNDS; i++) {
+  for (let i = 0; i < rounds; i++) {
     lengths.forEach(length => {
       permute(ring, pos, length)
-      pos = (pos + length + skipSize) % SIZE
+      pos = (pos + length + skipSize) % size
       skipSize++
     })
   }
@@ -49,7 +64,7 @@ function aFrayedKnot (lengths) {
 }
 
 function permute (r, p, l) {
-  assert(l < r.length, "can't permute more elements than exist in the list")
+  assert(l <= r.length, "can't permute more elements than exist in the list (" + l + ' requested of list ' + require('util').inspect(r) + ')')
   const pivot = Math.floor(l / 2)
   for (let i = 0; i < pivot; i++) {
     let near = (p + i) % r.length
@@ -64,6 +79,6 @@ function permute (r, p, l) {
 
 function buildRing (size) {
   const ring = new Array(size)
-  for (let e = 0; e < SIZE; e++) ring[e] = e
+  for (let e = 0; e < size; e++) ring[e] = e
   return ring
 }
