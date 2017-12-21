9b241da9690a3dd11917056b850a27d23f8eb5f1
Forrest L Norvell <ogd@aoaioxxysz.net>
2017-12-21 10:44:28 -1000

    make day 21 efficient

--- jennschifferscript/day-21.js
+++ jennschifferscript/day-21.js
@@ -8,8 +8,13 @@ const START = `.#.
                ..#
                ###`.split(/\n/).map(l => l.trim()).join('\n')
 
+const BUFFERS = {
+  2: makeSquare(2),
+  3: makeSquare(3)
+}
+
 assert.equal(
-  run1(
+  run(
     `../.# => ##./#../...
      .#./..#/### => #..#/..../..../#..#`,
     2
@@ -17,52 +22,59 @@ assert.equal(
   12
 )
 
-console.log('%s dots active after 5 rounds using rulebook', run1(input, 5))
+console.log('%s dots active after 5 rounds using rulebook', run(input, 5))
 
-console.log('%s dots active after 18 rounds using rulebook', run1(input, 18))
+console.log('%s dots active after 18 rounds using rulebook', run(input, 18))
 
-function run1 (input, iterations) {
+function run (input, iterations) {
   const rulebook = prepare(input)
 
-  let aht = START.split(/\n/).map(r => r.split(''))
+  let aht = Buffer.from(START.replace(/\n/g, ''), 'ascii')
   for (let i = 0; i < iterations; i++) {
-    if (aht.length % 2 === 0) {
-      const steps = aht.length / 2
-      const out = squarify(steps * 3)
-      for (let y = 0; y < steps; y++) {
-        for (let x = 0; x < steps; x++) {
-          const extracted = [
-            aht[y * 2].slice(x * 2, x * 2 + 2),
-            aht[y * 2 + 1].slice(x * 2, x * 2 + 2)
-          ].map(r => r.join('')).join('')
-          const replacement = rulebook.get(extracted)
-          for (let j = 0; j < 3; j++) {
-            out[y * 3 + j].splice(x * 3, 3, ...replacement[j])
-          }
-        }
-      }
-      aht = out
-    } else if (aht.length % 3 === 0) {
-      const steps = aht.length / 3
-      const out = squarify(steps * 4)
-      for (let y = 0; y < steps; y++) {
-        for (let x = 0; x < steps; x++) {
-          const extracted = [
-            aht[y * 3].slice(x * 3, x * 3 + 3),
-            aht[y * 3 + 1].slice(x * 3, x * 3 + 3),
-            aht[y * 3 + 2].slice(x * 3, x * 3 + 3)
-          ].map(r => r.join('')).join('')
-          const replacement = rulebook.get(extracted)
-          for (let j = 0; j < 4; j++) {
-            out[y * 4 + j].splice(x * 4, 4, ...replacement[j])
-          }
-        }
-      }
-      aht = out
-    } else throw new Error("Don't know how to handle a square of size " + aht.length)
+    const side = Math.sqrt(aht.length)
+    if (side % 2 === 0) {
+      aht = enhance(aht, 2, rulebook)
+    } else if (side % 3 === 0) {
+      aht = enhance(aht, 3, rulebook)
+    } else throw new Error("Don't know how to handle a square of size " + side)
   }
 
-  return aht.map(r => r.join('')).join('').split('').filter(e => e === '#').length
+  return aht.toString().replace(/\./g, '').length
+}
+
+function enhance (source, s, rulebook) {
+  const side = Math.sqrt(source.length)
+  const steps = side / s
+  const n = s + 1
+  let out = makeSquare(steps * n)
+  for (let y = 0; y < steps; y++) {
+    for (let x = 0; x < steps; x++) {
+      const replacement = rulebook.get(extract(source, s, x, y).toString())
+      tile(out, replacement, x, y)
+    }
+  }
+  return out
+}
+
+function extract (source, side, ox, oy) {
+  const fullSide = Math.sqrt(source.length)
+  let extracted = BUFFERS[side]
+  for (let k = 0; k < side; k++) {
+    for (let j = 0; j < side; j++) {
+      extracted[k * side + j] = source[ox * side + j + (oy * side + k) * fullSide]
+    }
+  }
+  return extracted
+}
+
+function tile (target, pattern, ox, oy) {
+  const side = Math.sqrt(pattern.length)
+  const increment = Math.sqrt(target.length) / side
+  for (let k = 0; k < side; k++) {
+    for (let j = 0; j < side; j++) {
+      target[ox * side + j + (oy * side + k) * (side * increment)] = pattern[j + k * side]
+    }
+  }
 }
 
 function prepare (input) {
@@ -72,19 +84,19 @@ function prepare (input) {
     const variations = new Set()
 
     const [, lhs, rhs] = line.match(/^([.#/]+)\s+=>\s+([.#/]+)$/)
-    const generator = rhs.split(/\//).map(r => r.split(''))
-    let ir = lhs.split('/')
+    const generator = rhs.replace(/\//g, '')
+    let ir = Buffer.from(lhs.replace(/\//g, ''), 'ascii')
     let ri = reflect(ir)
-    variations.add(ir.join('')); variations.add(ri.join(''))
+    variations.add(ir.toString()); variations.add(ri.toString())
     for (let i = 0; i < 3; i++) {
       ir = rotate(ir)
       ri = rotate(ri)
-      variations.add(ir.join('')); variations.add(ri.join(''))
+      variations.add(ir.toString()); variations.add(ri.toString())
     }
 
     for (let pattern of variations) {
       assert(!rulebook.get(pattern), 'each variation must be unique')
-      rulebook.set(pattern, generator)
+      rulebook.set(pattern, Buffer.from(generator, 'ascii'))
     }
   }
 
@@ -92,28 +104,28 @@ function prepare (input) {
 }
 
 function reflect (ir) {
-  const reflection = []
-  for (let row of ir) {
-    reflection.push(row.split('').reverse().join(''))
+  const side = Math.sqrt(ir.length)
+  let reflection = makeSquare(side)
+  for (let y = 0; y < side; y++) {
+    for (let x = 0; x < side; x++) {
+      reflection.writeUInt8(ir[side - x - 1 + y * side], x + y * side)
+    }
   }
   return reflection
 }
 
 function rotate (ir) {
-  const len = ir.length * ir.length
-  const lr = ir.map(r => r.split(''))
-  let out = squarify(ir.length)
-  for (let i = 0; i < len; i++) {
-    let x = i % ir.length
-    let y = Math.floor(i / ir.length)
-    let nx = ir.length - y - 1
-    let ny = x
-    out[ny][nx] = lr[y][x]
+  const side = Math.sqrt(ir.length)
+  let rotation = makeSquare(side)
+  for (let i = 0; i < ir.length; i++) {
+    let nx = side - Math.floor(i / side) - 1
+    let ny = i % side
+    rotation.writeUInt8(ir[i], nx + ny * side)
   }
 
-  return out.map(r => r.join(''))
+  return rotation
 }
 
-function squarify (size) {
-  return new Array(size).fill(0).map(r => new Array(size).fill('.'))
+function makeSquare (side) {
+  return Buffer.alloc(side * side, '.')
 }
